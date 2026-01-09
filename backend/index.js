1d9c8ca6956760bdcabcd17c3cc9c80adbb9d00e
hitaishi18-coder <hitaishi.8.8.8@gmail.com>
2026-01-09 20:48:43 +0530

    my commit

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://shopease-ai-support.onrender.com", // optional (if frontend same domain)
-      "https://shopease-ai-support.vercel.app",        // 🔁 replace with actual frontend
+      "*"
     ],
     methods: ["GET", "POST"],
     credentials: true,
